621963e134d7d078bbaa1a7d16ac81654c536a5b
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2021-12-01 15:01:56 -0500

    This is a change in CalculatorExecute

--- src/main/java/ec/epn/edu/git/CalculatorExecute.java
+++ src/main/java/ec/epn/edu/git/CalculatorExecute.java
@@ -10,5 +10,10 @@ public class CalculatorExecute {
 
         int subtraction = c.subtraction(7,1);
         System.out.println("c.subtraction(4,7): " + subtraction);
+
+        System.out.println("====== This is a change in the java project ======");
+        int division = c.division(111,23);
+        System.out.println("c.division(111,23): " + division);
+        System.out.println("======          End of the change           ======");
     }
 }
